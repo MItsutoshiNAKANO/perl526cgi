a0880ed83e9735c0809efe067e8e4db8a69034a7
Mitsutoshi Nakano <ItSANgo@gmail.com>
2023-12-10 09:36:48 +0900

    add pm

---
+++ perl/MyCGIApp.pm
@@ -0,0 +1,46 @@
+package MyCGIApp;
+
+use v5.26;
+use strict;
+use warnings;
+use utf8;
+
+use base qw(CGI::Application); # make sure this occurs before you load the plugin
+use HTML::Template;
+
+sub setup($) {
+    my $self = shift;
+    $self->header_props(-type => 'text/html; charset=UTF-8');
+    $self->start_mode('list_names') unless $self->get_current_runmode;
+    $self->mode_param('rm');
+    $self->run_modes(['list_names']);
+}
+
+sub list_names($) {
+    my $self = shift;
+    my $head = $self->load_tmpl('head.tmpl', utf8 => 1);
+    $head->param(LANG => 'ja');
+    $head->param(CHARSET => 'UTF-8');
+    $head->param(TITLE => '選択');
+    my $output = $head->output;
+
+    my $foot = $self->load_tmpl('foot.tmpl', utf8 => 1);
+    $output .= $foot->output;
+    return $output;
+}
+
+1;
+
+__END__
+
+=pod
+
+=encoding utf8
+
+=head1 NAME
+
+MyCGIApp - My CGI
+
+=head1 SEE ALSO
+
+ * <https://metacpan.org/pod/CGI::Application>
